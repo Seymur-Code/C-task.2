6a93d65351dffff95de84d302edc0e1e99bcaee9
Seymur-Abbasov <tu201806001@code.edu.az>
2021-10-26 19:59:35 +0400

    tasklarim

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace C_task._1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1_dən_100_a_qədər
+            /*for (int i = 1; i < 100; i++)
+
+            {
+                Console.WriteLine(i);
+            }*/
+            #endregion
+
+            #region 1_dən_10_a_qədər_ədədlərin_cəmi
+            /* var a = 0;
+             for (int i = 1; i <= 10; i++)
+             {
+                 Console.WriteLine("the number is:" + " " + i);
+                 a = a + i;
+             }
+             Console.WriteLine("the sum is:" + " " + a);*/
+            #endregion
+
+            #region operation
+            /*Console.WriteLine("1ci eded:" + " ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("operatoru daxil et: ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("2ci eded:" + " ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            if (operation == '+')
+                Console.WriteLine("cavab" + " " + (a + b));
+
+            else if (operation == '-')
+                Console.WriteLine("cavab" + " " + (a - b));
+            else if (operation == '*')
+                Console.WriteLine("cavab" + " " + (a * b));
+            else if (operation == '/')
+                Console.WriteLine("cavab" + " " + (a / b));
+            else
+                Console.WriteLine("sehv melumat");*/
+            #endregion
+
+            #region Task 1
+            /*  Console.WriteLine("ededi daxil edin:");
+              int a = Convert.ToInt32(Console.ReadLine());
+
+              if (a > 0)
+              {
+                  Console.WriteLine(a * 2);
+              }*/
+            #endregion
+
+            #region uzunluq_ve_sahe
+            /*Console.WriteLine("r");
+            int r = Convert.ToInt32(Console.ReadLine());
+            double l = 2 * Math.PI * r;
+            double s = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("l=" + l + "S=" + s);*/
+            #endregion
+
+            #region 1_dən_100_ə_qədər_tək_ədədləri
+            /*  for (int i = 1; i < 100; i += 2)
+              {
+                  Console.WriteLine(i);
+              }*/
+            #endregion\
+
+            #region 1_dən_10_a_qədər
+            /*for (int i = 1; i < 10; i++)
+
+            {
+                Console.WriteLine(i);
+            }*/
+            #endregion
+
+
+        }
+    }
+}
